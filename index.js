1d6907508fc3a94accf682727474ab946e274467
pilartorr <sauleturme@gmail.com>
2019-10-16 12:15:13 +0200

    connected Mongosee with MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // imported Express Library and passport file
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 
 // Express Server App
 const app = express();
 
 // import the Authentication Routes Function into our Server App
 require('./routes/authRoutes')(app);
+
 
 // use port 5000 unless there exists a preconfigured port
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -9,8 +9,8 @@ const keys = require('../config/keys');
 passport.use(
   new GoogleStrategy(
     {
-      clientID: 'keys.googleClientID',
-      clienSecret: 'keys.googleClientSecret',
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
