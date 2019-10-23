1151c886dafdd5421158746c13136c252ec8cdea
pilartorr <sauleturme@gmail.com>
2019-10-23 15:17:16 +0200

    fixed proxy in googlestrategy

--- services/passport.js
+++ services/passport.js
@@ -26,7 +26,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // find if the user already has a Google account
