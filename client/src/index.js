65132e82672fbc5a2b3579bac226afde40d0474d
pilartorr <sauleturme@gmail.com>
2019-11-06 17:18:00 +0100

    configured redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/App.js';
+import App from './components/App';
 
-ReactDom.render(<App />, document.queryselector('#root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
+);
