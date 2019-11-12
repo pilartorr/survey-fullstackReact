f718f942f18a0a542c8280320608bb41a60038dc
pilartorr <sauleturme@gmail.com>
2019-11-12 17:09:32 +0100

    configured stripe API and created the Payments component

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookiekey: process.env.COOKIE_KEY,
-  googleRedirectURI: process.env.REDIRECT_DOMAIN
+  googleRedirectURI: process.env.REDIRECT_DOMAIN,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
