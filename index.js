ca7041a484dae374640417d8eb176e44847d5f7f
pilartorr <sauleturme@gmail.com>
2019-11-14 13:39:26 +0100

    added some comments

--- index.js
+++ index.js
@@ -36,14 +36,16 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 // routing in production
-if(process.env.NODE_ENV === 'production'){
-  // Express will serve up production assets like main.js or main.css
-  app.use(express.static('client', 'build'));
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
-  // Express will serve up the index.html
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 // use port 5000 unless there exists a preconfigured port
